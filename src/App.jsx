18db284a059ea72186c5ae32322a87e18620a100
Eugene Lykhosherstov <lichiy.web.dvlp@gmail.com>
2025-01-22 16:07:29 +0200

    fix Chart.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import { useEffect, useState } from 'react'
-import Description from './components/Description/Description'
-import Options from './components/Options/Options'
-import Feedback from './components/Feedback/Feedback'
-import Notification from './components/Notification/Notification'
-import Chart from './components/Chart/Chart'
-import './App.css'
+import { useEffect, useState } from 'react';
+import Description from './components/Description/Description';
+import Options from './components/Options/Options';
+import Feedback from './components/Feedback/Feedback';
+import Notification from './components/Notification/Notification';
+import Chart from './components/Chart/Chart';
+import './App.css';
 
 export default function App() {
   const [clicks, setClicks] = useState(() => {
-    const savedFeedback = localStorage.getItem("saved-feedback-statistics");
+    const savedFeedback = localStorage.getItem('saved-feedback-statistics');
     if (savedFeedback !== null) {
       return JSON.parse(savedFeedback);
     }
     return { good: 0, neutral: 0, bad: 0 };
   });
 
-  const updateFeedback = (feedbackType) => {
+  const updateFeedback = feedbackType => {
     setClicks({
       ...clicks,
       [feedbackType]: clicks[feedbackType] + 1,
@@ -35,7 +35,7 @@ export default function App() {
   const positiveFeedback = Math.round((clicks.good / totalFeedback) * 100);
 
   useEffect(() => {
-    localStorage.setItem("saved-feedback-statistics", JSON.stringify(clicks));
+    localStorage.setItem('saved-feedback-statistics', JSON.stringify(clicks));
   }, [clicks]);
 
   return (
@@ -52,15 +52,17 @@ export default function App() {
         {!totalFeedback ? (
           <Notification />
         ) : (
-          <Feedback
-          good={clicks.good}
-          neutral={clicks.neutral}
-          bad={clicks.bad}
-            total={totalFeedback}
-            percentFeedback={positiveFeedback}
-          />
+          <div className="feedbackWrapper">
+            <Feedback
+              good={clicks.good}
+              neutral={clicks.neutral}
+              bad={clicks.bad}
+              total={totalFeedback}
+              percentFeedback={positiveFeedback}
+            />
+            <Chart clicks={clicks} />
+          </div>
         )}
-        <Chart data={clicks}  datasetsData={updateFeedback} />
       </div>
     </div>
   );
